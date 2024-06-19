09c76a52afe4ab25fa7118364143768c8d84a8c0
Anton <antosha.uvarov.90@mail.ru>
2024-06-20 00:54:37 +0300

    skeleton for news and bunner

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,25 +3,39 @@ import styles from './style.module.css';
 import { NewsBanner } from '../../components/NewsBanner/NewsBanner';
 import { getNews } from '../../api/apiNews';
 import { NewsList } from '../../components/NewsList/NewsList';
+import { Skeleton } from '../../components/Skeleton/Skeleton';
 
 export const Main = () => {
     const [news, setNews] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
         const fetchNews = async () => {
             try {
+                setIsLoading(true);
                 const data = await getNews();
                 console.log(data);
                 setNews(data.news);
             } catch (e) {
                 console.log(e);
+            } finally {
+                setIsLoading(false);
             }
         };
         fetchNews();
     }, []);
     return (
         <main className={styles.main}>
-            {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
-            <NewsList news={news} />
+            {news.length > 0 && !isLoading ? (
+                <NewsBanner item={news[0]} />
+            ) : (
+                <Skeleton count={1} type={'banner'} />
+            )}
+            {!isLoading ? (
+                <NewsList news={news} />
+            ) : (
+                <Skeleton count={10} type={'item'} />
+            )}
         </main>
     );
 };
