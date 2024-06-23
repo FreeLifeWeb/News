4a3170c1325f0b7bbeb1c5792437296458e7ce2f
Anton <antosha.uvarov.90@mail.ru>
2024-06-23 15:41:11 +0300

    add pagination

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,40 @@
+import style from './style.module.css';
+
+export const Pagination = ({
+    totalPages,
+    handleNextPage,
+    handlePreviosPage,
+    handleClickPage,
+    currentPage,
+}) => {
+    return (
+        <div className={style.pagination}>
+            <button
+                className={style.arrow}
+                onClick={() => handlePreviosPage()}
+                disabled={currentPage <= 1}
+            >
+                {'<'}
+            </button>
+            <div className={style.list}>
+                {[...Array(totalPages)].map((_, index) => (
+                    <button
+                        key={index}
+                        className={style.pageNumber}
+                        onClick={() => handleClickPage(index + 1)}
+                        disabled={index + 1 === currentPage}
+                    >
+                        {index + 1}
+                    </button>
+                ))}
+            </div>
+            <button
+                className={style.arrow}
+                onClick={() => handleNextPage()}
+                disabled={currentPage >= 10}
+            >
+                {'>'}
+            </button>
+        </div>
+    );
+};
